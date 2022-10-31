5b423adc799016ef33a679cb883c97ae3cdb1786
Anderson David <57516562+AndersonD4vid@users.noreply.github.com>
2022-10-31 12:02:41 -0300

    adicionando menu lateral

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("nav-mobile").style.width = "90%";
+  document.body.style.marginLeft = "90%";
+}
+
+function closeNav() {
+  document.getElementById("nav-mobile").style.width = "0";
+  document.body.style.marginLeft= "0";
+}
